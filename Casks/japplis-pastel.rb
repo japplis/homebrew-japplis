1f8662e5f507b1a5f1619f22bb533e2ce49b1cd1
Anthony Goubard <anthony.goubard@japplis.com>
2023-11-06 11:21:50 +0100

    Japplis Pastel version 1.0

--- Casks/japplis-pastel.rb
+++ Casks/japplis-pastel.rb
@@ -1,6 +1,6 @@
 cask "japplis-pastel" do
   version "1.0"
-  sha256 "8da6e44144fddf6434460928711ebbb233101864c9ade3d5f53290aa56f70b5e"
+  sha256 "99a948e69197e06e97f8107d58c06f5c344ae0e2419d6e987943e623f59aacc8"
 
   url "https://www.japplis.com/decoration/pastel/versions/Pastel-#{version}.dmg"
   name "Japplis Pastel"
