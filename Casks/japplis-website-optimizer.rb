6b82e1fa45cc6373fb223f4dddf63fe5566d2db5
Anthony Goubard <anthony.goubard@japplis.com>
2024-02-17 11:50:45 +0100

    Japplis Website Optimizer version 4.2

--- Casks/japplis-website-optimizer.rb
+++ Casks/japplis-website-optimizer.rb
@@ -1,6 +1,6 @@
 cask "japplis-website-optimizer" do
-  version "4.1"
-  sha256 "7142ea6d7e60c07154f85fa93121fb110ee1ede37dd5aef10832fe75acc22782"
+  version "4.2"
+  sha256 "03695afe16917374c09d319ca02d118906fc31ec78a52693718c05e967bb773d"
 
   url "https://www.japplis.com/website-optimizer/versions/WebsiteOptimizer-#{version}.dmg"
   name "Japplis Website Optimizer"
