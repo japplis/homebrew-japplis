809f25058529a5f86e148f41b6bb6636a7b0eab3
Anthony Goubard <anthony.goubard@japplis.com>
2024-02-22 13:44:18 +0100

    Screen Highlighter Pro version 1.1

--- Casks/screen-highlighter-pro.rb
+++ Casks/screen-highlighter-pro.rb
@@ -1,6 +1,6 @@
 cask "screen-highlighter-pro" do
-  version "1.0"
-  sha256 "5d29665de614aa16ee09efae91792e8ff87a013bc91715067935e8bfc5323b97"
+  version "1.1"
+  sha256 "c05b88a4b25a7d2cbfaba97ee79e7478cc5f97832a88d65d78a5044503e64755"
 
   url "https://www.japplis.com/screen-highlighter/pro/versions/ScreenHighlighterPro-#{version}.dmg"
   name "Screen Highlighter Pro"
