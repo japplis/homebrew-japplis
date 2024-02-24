608d07b4d9ed0faa1af1ab1dd0fc1d360516cafb
Anthony Goubard <anthony.goubard@japplis.com>
2024-02-24 16:16:47 +0100

    Screenshot Crop version 1.1

--- Casks/screenshot-crop.rb
+++ Casks/screenshot-crop.rb
@@ -1,6 +1,6 @@
 cask "screenshot-crop" do
-  version "1.0"
-  sha256 "53fe10eeb7bfc883b02a4180d95a40bdd67b2341d76194647eb37ed3f1c4b3cf"
+  version "1.1"
+  sha256 "439e6cc78080878fe0f181bfc334d22bebcfc390f2192bd0b9bbe9af388d8dba"
 
   url "https://www.japplis.com/screenshot-crop/versions/ScreenshotCrop-#{version}.dmg"
   name "Screenshot Crop"
