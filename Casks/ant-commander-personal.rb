01cabe282642fc97a7fb62a20b2e5d6ad0dddcf1
Anthony Goubard <anthony.goubard@japplis.com>
2024-03-06 10:53:59 +0100

    Ant Commander Personal version 4.7

--- Casks/ant-commander-personal.rb
+++ Casks/ant-commander-personal.rb
@@ -1,6 +1,6 @@
 cask "ant-commander-personal" do
-  version "4.6"
-  sha256 "e2390c06f00311b3db0dc10cd3c8def784a246c4c0eb1eb9a1b4b0e8671f765d"
+  version "4.7"
+  sha256 "33d32dec0e15da8acaf42d9df37db4e9426f3cc18d08e9a8fa60456c512d7e5f"
 
   url "https://www.antcommander.com/personal/versions/AntCommander-#{version}.dmg"
   name "Ant Commander Personal"
