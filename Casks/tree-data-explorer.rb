d463a13afa723aab6b449ad83668df845033e627
Anthony Goubard <anthony.goubard@japplis.com>
2024-10-07 17:57:42 +0200

    New version: Tree Data Explorer version 1.2.5

--- Casks/tree-data-explorer.rb
+++ Casks/tree-data-explorer.rb
@@ -1,6 +1,6 @@
 cask "tree-data-explorer" do
-  version "1.1"
-  sha256 "9eb09419198aee734c81b3a42595f337342f46921d4fa5f5cf6604bbf7f50c00"
+  version "1.2.5"
+  sha256 "781833164e3170ad4c3f773547404a62f69445e0822970866028a1c953f7e27e"
 
   url "https://www.japplis.com/tree-data-explorer/versions/TreeDataExplorer-#{version}.dmg"
   name "Tree Data Explorer"
