1bcf364a77b9eed2837776c39520b298d9b4e0dd
Anthony Goubard <anthony.goubard@japplis.com>
2025-01-17 09:55:04 +0100

    New version: Japplis Toolbox version 5.9

--- Casks/japplis-toolbox.rb
+++ Casks/japplis-toolbox.rb
@@ -1,6 +1,6 @@
 cask "japplis-toolbox" do
-  version "5.8"
-  sha256 "9a868a031edc905bb70258a57fd936d28c2fae9d692858f68d6eb12945411e9d"
+  version "5.9"
+  sha256 "98314bf438446cb3f14b4b40df8e3c224c5e82850d72b036f202d8e56192b6c3"
 
   url "https://www.japplis.com/toolbox/versions/Toolbox-#{version}.dmg"
   name "Japplis Toolbox"
