0f16ef823425a8b5eb60f79a2ce2af5eb24ecda7
Anthony Goubard <anthony.goubard@japplis.com>
2025-03-11 16:38:05 +0100

    New version: The Desktop Watchmaker Widget version 1.4

--- Casks/the-desktop-watchmaker-widget.rb
+++ Casks/the-desktop-watchmaker-widget.rb
@@ -1,6 +1,6 @@
 cask "the-desktop-watchmaker-widget" do
-  version "1.3"
-  sha256 "6a36cd5a24873a373fc898b1782a5939c8ec5319da8d94adb5159f50d83773a6"
+  version "1.4"
+  sha256 "4543d4680d6af19b13f30ce51d99742b70e2383eaf17b07e539703af60e106a4"
 
   url "https://www.japplis.com/desktop-watchmaker/widget/versions/DesktopWatchmakerWidget-#{version}.dmg"
   name "The Desktop Watchmaker Widget"
