4f507fc0a29fc6e63d251988ac113f1b71e9ef53
Anthony Goubard <anthony.goubard@japplis.com>
2025-04-03 17:10:21 +0200

    New version: JLearnIt version 6.9

--- Casks/jlearnit.rb
+++ Casks/jlearnit.rb
@@ -1,6 +1,6 @@
 cask "jlearnit" do
-  version "6.7"
-  sha256 "f5c1de72866a59466b6ddad21a896447374ec6416ff67939155e4def2ea1aaa3"
+  version "6.9"
+  sha256 "29b378765a7a71d7d3128812a9461fecc8f6feaaa5f79c67219f72df385b22c6"
 
   url "https://www.jlearnit.com/versions/JLearnIt-#{version}.dmg"
   name "JLearnIt"
