66795b57cc05d0ff27bb1b41bdb04939ee8a5d26
Anthony Goubard <anthony.goubard@japplis.com>
2025-05-28 11:06:36 +0200

    New version: Japplis Clipboard History Pro version 1.3

--- Casks/japplis-clipboard-history-pro.rb
+++ Casks/japplis-clipboard-history-pro.rb
@@ -1,6 +1,6 @@
 cask "japplis-clipboard-history-pro" do
-  version "1.2"
-  sha256 "d63257752bab55048de12e7f853eec38195dbfc5a1950eabf2785d16915688c5"
+  version "1.3"
+  sha256 "11a8633ff3d6544af4b6b183640399962b10261d291696ee8ffb86ec8a05c9da"
 
   url "https://www.japplis.com/clipboard-history/pro/versions/ClipboardHistoryPro-#{version}.dmg"
   name "Japplis Clipboard History Pro"
